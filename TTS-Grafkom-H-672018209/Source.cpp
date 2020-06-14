eec44ee571bab73d4998c197c16a30e010d4556c
if_balino <balino.ical@gmail.com>
2020-06-14 10:23:11 +0700

    create first line

--- TTS-Grafkom-H-672018209/Source.cpp
+++ TTS-Grafkom-H-672018209/Source.cpp
@@ -6,6 +6,17 @@
 
 
 void display() {
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_LINES);
+	glVertex2d(10.0, 30.0);
+	glVertex2d(10.0, 610.0);
+	glEnd();
+
+
+	glFlush();
 
 }
 
@@ -16,13 +27,13 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); //single buffer unutk menampilkan window | warna rgb
 	glutInitWindowSize(640, 480); //size, 640px x 480px
 	glutInitWindowPosition(10, 10); //posisi windows
-	glutCreateWindow("Titik Perpotongan by IFbalino");
+	glutCreateWindow("BATIK TORAJA by ifbalino");
 
 	//inisialisasi glut
-	glClearColor(0.0, 1.0, 1.0, 0.0);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glPointSize(10);
-	glLineWidth(4.0f);
+	glLineWidth(5.0f);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity(); // dihitung dengan matrix identitas
 	gluOrtho2D(0.0, 640.0, 0.0, 480.0); // untuk memberikan sistem koordinat kepada windows yang kita buat
